8f5748eb6cdab2e05ac5a271a1d5b47d33d97112
Peter Prog <peter.prog@yahoo.com>
2022-08-06 20:03:38 +0800

    api: envs

--- ui/public/env.js
+++ ui/public/env.js
@@ -1,3 +1,6 @@
-UI_SERVER_PORT=8000
-UI_API_ENDPOINT_LOCAL=http://localhost:3100/graphql
-UI_API_ENDPOINT=https://07-02-api.vercel.app/graphql
+
+window.ENV = {
+	UI_SERVER_PORT=8000
+	UI_API_ENDPOINT_LOCAL=http://localhost:3100/graphql
+	UI_API_ENDPOINT=https://07-02-api.vercel.app/graphql
+}
