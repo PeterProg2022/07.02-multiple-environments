de2685bd077d7e34ecb917b5d758f292649e5745
Peter Prog <peter.prog@yahoo.com>
2022-08-06 19:25:04 +0800

    api: envs

--- ui/uiserver.js
+++ ui/uiserver.js
@@ -12,7 +12,7 @@ app.get('/env.js', function(req, res) {
   res.send(`window.ENV = ${JSON.stringify(env)}`)
 })
 
-const port = process.env.UI_SERVER_PORT || 8001;
+const port = process.env.UI_SERVER_PORT || 8000;
 
 app.listen(port, function () {
   console.log(`UI started on port ${port}`);
